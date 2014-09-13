b9360e9f002a16c239ab5207a9fbcfc4e5bd4ac2
James Barlow <james@cloudshout.co.uk>
2014-09-13 06:16:45 +0100

    Cleanup simple nav

--- js/six.js
+++ js/six.js
@@ -7,29 +7,7 @@ google.maps.event.addDomListener(window, 'load', function() {
   $map_one = new google.maps.Map(
     document.getElementById('map-canvas-one'),{
       center: $rightHere,
-      zoom: 8,
-      streetViewControl: true
+      zoom: 8
       }
     );
-
-
-  $map_two = new google.maps.Map(
-    document.getElementById('map-canvas-two'),{
-      center: $rightHere,
-      zoom: 20,
-      mapTypeId: google.maps.MapTypeId.SATELLITE,
-      streetViewControl: false
-      }
-    );
-  $panorama = $map_two.getStreetView();
-
-  $panorama.setPosition($rightHere);
-  $panorama.setPov({
-    heading: 45,
-    pitch:-5}
-  );
-});
-
-$('#toggleStreetView').on('click',function(){
-    $panorama.setVisible(!$panorama.getVisible());
 });
